53bea1b1c956311c6aa51912e83c8dd66f614f48
Sushanth Ramakrishna <sushanthr@gmail.com>
2021-04-12 17:03:41 -0700

    Add jest test file

---
+++ src/components/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+describe('Header', () => {
+  it('renders children when passed in', () => {
+    // Add tests here.
+  });
+
+  it('renders props when passed in', () => {
+    // Add tests here
+  });
+});
+
